e4c8d7c5c4ca25ea0b9710d07aafcc4c093ee16e
menglingjie110@163.com <menglingjie110@163.com>
2017-11-06 16:35:19 +0800

    20171106v1

--- authority-webapp/src/main/webapp/statics/js/sys/permission/popedom.js
+++ authority-webapp/src/main/webapp/statics/js/sys/permission/popedom.js
@@ -1,14 +1,6 @@
 /**
  * 分配角色
  */
-//var popedom={}
-//
-//popedom.selectRole=function(){
-//	alert(33);
-//}
-//popedom.noselectRole=function(){
-//	alert(44);
-//}
 $(document).ready(function() {
 	$("#roleId option:first,#popedomIds option:first").attr("selected",true);
 	$("#selects").live("click",function(){
@@ -35,15 +27,14 @@ $(document).ready(function() {
 	$("#popedomIds").live("dblclick",function(){
 		var items = $("#popedomIds option");
 	    var so = $("#popedomIds option:selected");
-			if(so.get(so.length-1).index == items.length-1) so.prev().attr("selected",true);
-			else so.next().attr("selected",true);
-			$("#roleId").append(so);
+		if(so.get(so.length-1).index == items.length-1) so.prev().attr("selected",true);
+		else so.next().attr("selected",true);
+		$("#roleId").append(so);
 	});
-	
 });
 function save(form, action) {
-		$("#popedomIds option:all").attr("selected",true);
-		form.action = action;
-		form.method = "post";
-		form.submit();
+	$("#popedomIds option:all").attr("selected",true);
+	form.action = action;
+	form.method = "post";
+	form.submit();
 }
