8c1f723a56b4bed28f3c40085a8e8a282129e37d
menglingjie110@163.com <menglingjie110@163.com>
2017-11-06 17:37:26 +0800

    v1

--- document/authority.sql
+++ document/authority.sql
@@ -1,8 +1,9 @@
 /*
 SQLyog Enterprise - MySQL GUI v7.15 
-MySQL - 5.7.17-log : Database - ecbiz
+MySQL - 5.5.45-log : Database - authority
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -11,9 +12,9 @@ MySQL - 5.7.17-log : Database - ecbiz
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`ecbiz` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`authority` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `ecbiz`;
+USE `authority`;
 
 /*Table structure for table `sys_logs` */
 
