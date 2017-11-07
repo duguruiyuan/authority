29d346a3f26f9d7bad7d050999ac791e92595da2
menglingjie110@163.com <menglingjie110@163.com>
2017-11-07 08:44:13 +0800

    20171107v1

--- authority-webapp/src/main/webapp/statics/js/sys/permission/sysLogClear.js
+++ authority-webapp/src/main/webapp/statics/js/sys/permission/sysLogClear.js
@@ -1,17 +1,18 @@
-/**
- * 
- */
+//----------------------------------------------------//
+//----------------------> 日志管理       @孟令杰  <--------------//
+//----------------------------------------------------//
 var sysLogClear={};
 
 sysLogClear.opcal= function(){
     $.calendar.Show();
 }
+
 // 页面初始化执行
 $(document).ready(function() {
 	$('#startTime').calendar();
 	$('#endTime').calendar();
 });
-
+//----------------------> 清楚日志      <--------------//
 sysLogClear.logClear=function(){
 	var startTime=$('#startTime').val();
 	var endTime=$('#startTime').val();
