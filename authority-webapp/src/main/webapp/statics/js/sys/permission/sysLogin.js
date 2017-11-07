587e02a77e17f87d4d2510e62c597d741ee96d65
menglingjie110@163.com <menglingjie110@163.com>
2017-11-07 08:47:16 +0800

    20171107v1

--- authority-webapp/src/main/webapp/statics/js/sys/permission/sysLogin.js
+++ authority-webapp/src/main/webapp/statics/js/sys/permission/sysLogin.js
@@ -1,26 +1,22 @@
-/**
- * 
- */
-var sysLogin={};
+//----------------------------------------------------------------------//	  
+//------------------------>	  后台登录界面     @孟令杰         <----------------------------//
+//----------------------------------------------------------------------//
+var sysLogin={params:{},username:null,pwssword:null};
 sysLogin.toLogin=function(){
-	var username=$("#username").val();
-	var pwssword=$("#password1").val();
-	var a=username.length; var b=pwssword.length;
-	
+	this.username=$("#username").val();
+	this.pwssword=$("#password1").val();
+	var a=this.username.length; var b=this.pwssword.length;
 	if(a==0||b==0){
 		alert("用户名和密码不能为空 !");
 		return;
 	}
-	var params = {
-			uname:username,
-			password:pwssword
-	};
+	this.params = {uname:this.username,password:this.pwssword};
 	$.ajax({
 		url : "/sysuser/findSysUser",
 		dataType : 'json',
 		type : 'post',
 		scriptCharset :'utf-8',
-		data : params,
+		data : this.params,
 		success : function(data) {
 			if (data != '-1') {
 				location.href = "index.htm";
